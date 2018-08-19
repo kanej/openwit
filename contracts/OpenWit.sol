2d21daeb4f36de7c68a6462e2b384e22d4a56ea2
John Kane <john@kanej.me>
2018-08-19 17:47:38 +0100

    style(contract): Fix linter issue with import

--- contracts/OpenWit.sol
+++ contracts/OpenWit.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract OpenWit is Ownable {
   
